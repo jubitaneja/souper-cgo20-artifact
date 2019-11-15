c2cb587273f1a4085afeccdf5483b856430afc6d
jubitaneja <jubi@cs.utah.edu>
2019-11-15 21:17:20 +0000

    fix

--- performance/test/bzip2.sh
+++ performance/test/bzip2.sh
@@ -6,17 +6,21 @@ PRECISE_BZIP2=$BZIP2_DIR/precise
 
 RESULT_DIR=/usr/src/artifact-cgo/performance/test/
 
-for i in 0 .. 3; do
+for i in {1..3}; do
+	echo "Iteration #$i: computing baseline compression time";
 	echo "baseline-compression time" >> $RESULT_DIR/result-bzip.txt;
 	(time -p $BASELINE_BZIP2/bzip2 -k -f $RESULT_DIR/1gb) 2>> $RESULT_DIR/result-bzip.txt;
 	echo
+	echo "Iteration #$i: computing precise compression time";
 	echo "precise-compression time" >> $RESULT_DIR/result-bzip.txt;
 	(time -p $PRECISE_BZIP2/bzip2 -k -f $RESULT_DIR/1gb) 2>> $RESULT_DIR/result-bzip.txt;
 	echo
 	echo
+	echo "Iteration #$i: computing baseline decompression time";
 	echo "baseline-decompression time" >> $RESULT_DIR/result-bzip.txt;
 	(time -p $BASELINE_BZIP2/bzip2 -k -d -f $RESULT_DIR/1gb.bz2) 2>> $RESULT_DIR/result-bzip.txt;
 	echo
+	echo "Iteration #$i: computing precise compression time";
 	echo "precise-decompression time" >> $RESULT_DIR/result-bzip.txt;
 	(time -p $PRECISE_BZIP2/bzip2 -k -d -f $RESULT_DIR/1gb.bz2) 2>> $RESULT_DIR/result-bzip.txt;
 	echo
