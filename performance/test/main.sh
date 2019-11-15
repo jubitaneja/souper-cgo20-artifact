8b36fb3e8454c21e6136fbf0df546e5be2a8db7a
Jubi Taneja <jubi@cs.utah.edu>
2019-11-15 15:26:44 -0800

    pretty

--- performance/test/main.sh
+++ performance/test/main.sh
@@ -49,3 +49,43 @@ echo
 
 ${RESULT_DIR}/stockfish.sh
 
+echo
+echo
+echo "All results are saved in individual result
+files."
+echo
+ls result-*.txt
+echo
+echo"Now, we will quickly get the
+final numbers to compare performance of baseline
+with precise version."
+echo
+echo "==================================="; 
+echo "Final result of bzip2";
+echo "==================================="; 
+echo
+python time-zip.py result-bzip*.txt
+echo
+
+echo "==================================="; 
+echo "Final result of gzip";
+echo "==================================="; 
+echo
+python time-zip.py result-gzip.txt
+echo
+
+echo "==================================="; 
+echo "Final result of SQLite";
+echo "==================================="; 
+echo
+python time-sqlite.py result-sqlite.txt
+echo
+
+echo "==================================="; 
+echo "Final result of Stockfish";
+echo "==================================="; 
+echo
+python time-stockfish.py result-stockfish.txt
+echo
+
+echo "Performance evaluation is all done!!!"
